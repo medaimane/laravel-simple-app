5fa42c0dddf281b5678d4fd675468597d791f4fd
unknown <skhairimedaimane@gmail.com>
2018-10-03 11:49:58 +0100

    make foreach loop to show home posts

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -35,44 +35,24 @@
 
         <!-- START THE FEATURETTES -->
 
-        <hr class="featurette-divider">
+        {{-- <hr class="featurette-divider"> --}}
 
-        <div class="row featurette">
-            <div class="col-md-7">
-                <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        @if (count($posts) > 0)
+         @foreach ($posts as $post)
+            <div class="row featurette">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading">{{$post->title}}</h2>
+                    <h4><span class="text-muted">{{$post->description}}</span></h4>
+                    <p class="lead">{!!$post->body!!}</p>
+                </div>
+                <div class="col-md-5 mt-5">
+                    <img class="featurette-image img-fluid mx-auto" src="{{asset('storage/posts/'.$post->cover_image)}}" alt="Generic placeholder image">
+                </div>
             </div>
-            <div class="col-md-5">
-                <img class="featurette-image img-fluid mx-auto" src="{{asset('storage/posts/'.$posts[0]->cover_image)}}" alt="Generic placeholder image">
-            </div>
-        </div>
 
-        <hr class="featurette-divider">
-
-        <div class="row featurette">
-            <div class="col-md-7 order-md-2">
-                <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-            </div>
-            <div class="col-md-5 order-md-1">
-                <img class="featurette-image img-fluid mx-auto" src="{{asset('storage/posts/'.$posts[1]->cover_image)}}" alt="Generic placeholder image">
-            </div>
-        </div>
-
-        <hr class="featurette-divider">
-
-        <div class="row featurette">
-            <div class="col-md-7">
-                <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-            </div>
-            <div class="col-md-5">
-                <img class="featurette-image img-fluid mx-auto" src="{{asset('storage/posts/'.$posts[2]->cover_image)}}" alt="Generic placeholder image">
-            </div>
-        </div>
-
-        <hr class="featurette-divider">
-
+            <hr class="featurette-divider">
+         @endforeach
+        @endif
         <!-- /END THE FEATURETTES -->
 
     </div> <!--/.container -->
